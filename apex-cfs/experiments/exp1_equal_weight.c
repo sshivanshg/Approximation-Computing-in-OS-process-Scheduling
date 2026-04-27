effe0107199980009a9347861438dc884b524ace
Vivek Vishnoi <vivek.23csai@nst.rishihood.edu.in>
2026-04-27 12:21:02 +0530

    Step 8: add Makefile and experiment stubs

---
+++ apex-cfs/experiments/exp1_equal_weight.c
@@ -0,0 +1,12 @@
+#include "../src/task.h"
+#include "../src/cfs_exact.h"
+#include "../src/approx_bsa.h"
+#include "../src/approx_clti.h"
+#include "../src/approx_apaf.h"
+#include "../src/metrics.h"
+
+int main(void) {
+    printf("EXP1: placeholder\n");
+    printf("Run after Step 9 implementation\n");
+    return 0;
+}
